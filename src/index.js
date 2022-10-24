00abc8ffb0700a8b9354ba3b8771b1bb6de2eddc
yifeitai <neversaynever1022@gmail.com>
2022-10-24 10:10:14 -0400

    2560a2

--- src/index.js
+++ src/index.js
@@ -2,46 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
-
-const books = [
-  {
-    title: '11111',
-    text: 'qqqqqq',
-  },
-  {
-    title: '22222',
-    text: 'wwwwww',
-  },
-];
-
-function BookList() {
-  return (
-    <section className='book'>
-      {books.map((book) => {
-        const { title, text } = book;
-        // console.log(book);
-        return <Book book={book}></Book>;
-      })}
-    </section>
-  );
-}
-
-function Book(props) {
-  console.log(props);
-  const { title, text } = props.book;
-  return (
-    <article>
-      <h1>{title}</h1>
-      <h4>{text}</h4>
-    </article>
-  );
-}
-
+import { AppProvider } from './context';
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
-    <BookList />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
